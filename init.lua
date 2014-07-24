cdf68785fde6bb8656cf98dc2de9192966acdf19
Chris Jones <cmsj@tenshu.net>
2014-07-24 15:45:36 -0400

    Rename allfinder back to appfinder

--- init.lua
+++ init.lua
@@ -1,10 +1,10 @@
--- allfinder.lua v2014.07.07
+-- appfinder.lua v2014.07.07
 -- cmsj@tenshu.net
 -- Utility functions to aid finding:
 --   * Applications
 --   * Windows
 
-ext.allfinder = {}
+ext.appfinder = {}
 
 -- Internal function to search all windows using a matching function
 local function find_window_from_function(fn)
@@ -26,7 +26,7 @@ end
 --      name: Application name (e.g. "Safari")
 --  Returns:
 --      application object or nil
-function ext.allfinder.app_from_name(name)
+function ext.appfinder.app_from_name(name)
     return fnutils.find(application.runningapplications(), function(app) return app:title() == name end)
 end
 
@@ -35,7 +35,7 @@ end
 --      title: Window title (e.g. "Activity Monitor (All Processes)")
 --  Returns:
 --      application object or nil
-function ext.allfinder.app_from_window_title(title)
+function ext.appfinder.app_from_window_title(title)
     return find_application_from_window(title, function(win) return win:title() == title end)
 end
 
@@ -48,7 +48,7 @@ end
 --      For more about Lua patterns, see:
 --       http://lua-users.org/wiki/PatternsTutorial
 --       http://www.lua.org/manual/5.2/manual.html#6.4.1
-function ext.allfinder.app_from_window_title_pattern(pattern)
+function ext.appfinder.app_from_window_title_pattern(pattern)
     return find_application_from_window(pattern, function(win) return string.match(win:title(), pattern) end)
 end
 
@@ -57,6 +57,6 @@ end
 --    title: Window title (e.g. "Activity Monitor (All Processes)")
 --  Returns:
 --    window object or nil
-function ext.allfinder.window_from_window_title(title)
+function ext.appfinder.window_from_window_title(title)
     return find_window_from_function(function(win) return win:title() == title end)
 end
